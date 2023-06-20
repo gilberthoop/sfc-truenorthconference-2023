74800bbaa6bb58e723cd6d0a6a8962f63273a1c2
William Go <gilberthoop@Williams-MacBook-Pro.local>
2023-06-19 19:33:21 -0700

    Create registration statistics chart

---
+++ src/components/registration/RegistrationStats.tsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { RegionStats } from "@/utils/types";
+import { useState, useEffect } from "react";
+import { CircularProgress } from "@mui/material";
+import AppBarGraph from "../AppBarGraph";
+
+function RegistrationStats() {
+  const API_ENDPOINT_URL = "/api/statistics";
+  const [regionStats, setRegionStats] = useState<RegionStats[]>();
+
+  async function fetchRegionStats() {
+    try {
+      const response = await axios.get(API_ENDPOINT_URL);
+      const statsResult = response.data?.regionStatistics;
+
+      setRegionStats(statsResult);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchRegionStats();
+  }, []);
+
+  return (
+    <main className="my-10">
+      {regionStats?.length ? (
+        <AppBarGraph
+          title={"Number of Registered Participants"}
+          data={regionStats}
+        />
+      ) : (
+        <div className="flex justify-center">
+          <CircularProgress size={50} color="info" />
+        </div>
+      )}
+    </main>
+  );
+}
+
+export default RegistrationStats;
