e56d775e28d13e8404fefa446ae4d5cd1d5170f0
William Gilbert Go <williamgilbertgo@gmail.com>
2023-06-28 16:01:47 -0700

    Redesign promo photos structure (#32)

--- src/components/landing/Promo.tsx
+++ src/components/landing/Promo.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import WinRegistrationImage from "@/assets/win_free_registration.webp";
 import ReducedRegistrationFee from "@/assets/reduced_reg_fee.webp";
 import RegisterNowImage from "@/assets/register_now.webp";
+import RegisterImage from "@/assets/have_you_registered.webp";
 
 function Promo() {
   const images = [
     { image: WinRegistrationImage, alt: "Win a Free Registration" },
     { image: ReducedRegistrationFee, alt: "Reduced Registration Fee" },
     { image: RegisterNowImage, alt: "Register Now" },
+    { image: RegisterImage, alt: "Have you registered?" },
   ];
 
   return (
