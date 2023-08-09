00d2dc1fe77738be453db8591d45d433cb2e3511
William Gilbert Go <williamgilbertgo@gmail.com>
2023-08-09 13:28:04 -0700

    Rearrange schedule contents styling (#51)

--- src/components/landing/schedule/ProgramSchedule.tsx
+++ src/components/landing/schedule/ProgramSchedule.tsx
@@ -3,10 +3,8 @@ import { PROGRAM_SCHEDULE } from "@/utils/global-values";
 
 const ProgramSchedule: React.FC = () => {
   return (
-    <section className="py-14 px-0 sm:px-20 md:px-32 xl:px-80">
-      <h1 className="pt-8 text-center text-white text-3xl md:text-5xl">
-        Program Schedule
-      </h1>
+    <section className="schedule__container">
+      <h1>Program Schedule</h1>
       {PROGRAM_SCHEDULE.map(({ date, timetable, imageDetails }) => (
         <Schedule
           key={date}
